ed717c6b5e869132ef8e844cfa480e184c86e2e9
Itumeleng <Abramsikn@gmail.com>
2018-08-16 13:38:07 +0200

    [15/08/2018]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './template/navbar/navbar.component';
+import { SignupComponent } from './template/pages/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    MDBBootstrapModule.forRoot(),
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '', 
+        component: AppComponent 
+      },
+      { 
+        path: 'signup', 
+        component: SignupComponent
+      },
+      /*{ 
+        path: '**', 
+        component: NotFoundComponent
+      }, */
+    ])
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
