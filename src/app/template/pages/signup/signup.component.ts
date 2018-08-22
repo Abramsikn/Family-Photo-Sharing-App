88f4f923782758f4c7cfad736f6bfa90dfd6f6e5
Itumeleng <Abramsikn@gmail.com>
2018-08-22 16:07:59 +0200

    [22/08/2018]

--- src/app/template/pages/signup/signup.component.ts
+++ src/app/template/pages/signup/signup.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router  } from '@angular/router';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -12,6 +11,9 @@ export class SignupComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form : NgForm) { 
   }
 
 }
